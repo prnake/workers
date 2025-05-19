2f2df76de3556d05d5db3e504add09b42f66f862
papersnake <prnake@gmail.com>
2025-05-20 00:54:08 +0800

    Update minhash_cc_nemo.py

--- streamlit/minhash_cc_nemo.py
+++ streamlit/minhash_cc_nemo.py
@@ -1,11 +1,91 @@
 import streamlit as st
-import mmh3
 
 import re
 import unicodedata
 import struct
 import hashlib
 import numpy as np
+
+import sys as _sys
+if (_sys.version_info > (3, 0)):
+    def xrange( a, b, c ):
+        return range( a, b, c )
+    def xencode(x):
+        if isinstance(x, bytes) or isinstance(x, bytearray):
+            return x
+        else:
+            return x.encode()
+else:
+    def xencode(x):
+        return x
+del _sys
+
+def mmh3_hash_py(key, seed=0x0, signed=False):
+    """Implements 32bit murmur3 hash."""
+
+    key = bytearray(xencode(key))
+
+    def fmix(h):
+        h ^= h >> 16
+        h = (h * 0x85EBCA6B) & 0xFFFFFFFF
+        h ^= h >> 13
+        h = (h * 0xC2B2AE35) & 0xFFFFFFFF
+        h ^= h >> 16
+        return h
+
+    length = len(key)
+    nblocks = int(length / 4)
+
+    h1 = seed
+
+    c1 = 0xCC9E2D51
+    c2 = 0x1B873593
+
+    # body
+    for block_start in xrange(0, nblocks * 4, 4):
+        # ??? big endian?
+        k1 = (
+            key[block_start + 3] << 24
+            | key[block_start + 2] << 16
+            | key[block_start + 1] << 8
+            | key[block_start + 0]
+        )
+
+        k1 = (c1 * k1) & 0xFFFFFFFF
+        k1 = (k1 << 15 | k1 >> 17) & 0xFFFFFFFF  # inlined ROTL32
+        k1 = (c2 * k1) & 0xFFFFFFFF
+
+        h1 ^= k1
+        h1 = (h1 << 13 | h1 >> 19) & 0xFFFFFFFF  # inlined ROTL32
+        h1 = (h1 * 5 + 0xE6546B64) & 0xFFFFFFFF
+
+    # tail
+    tail_index = nblocks * 4
+    k1 = 0
+    tail_size = length & 3
+
+    if tail_size >= 3:
+        k1 ^= key[tail_index + 2] << 16
+    if tail_size >= 2:
+        k1 ^= key[tail_index + 1] << 8
+    if tail_size >= 1:
+        k1 ^= key[tail_index + 0]
+
+    if tail_size > 0:
+        k1 = (k1 * c1) & 0xFFFFFFFF
+        k1 = (k1 << 15 | k1 >> 17) & 0xFFFFFFFF  # inlined ROTL32
+        k1 = (k1 * c2) & 0xFFFFFFFF
+        h1 ^= k1
+
+    # finalization
+    unsigned_val = fmix(h1 ^ length)
+    if not signed:
+        return unsigned_val
+
+    if unsigned_val & 0x80000000 == 0:
+        return unsigned_val
+    else:
+        return -((unsigned_val ^ 0xFFFFFFFF) + 1)
 
 URL_PATTERN = re.compile(r'https?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 
@@ -46,7 +126,7 @@ class MinHash:
     def __init__(self, num_perm=260, seed=42, hashfunc=None):
         self.num_perm = num_perm
         self.seed = seed
-        self.hashfunc = hashfunc or (lambda x: mmh3.hash(x, seed=seed, signed=False))
+        self.hashfunc = hashfunc or (lambda x: mmh3_hash_py(x, seed=seed, signed=False))
         self.hashvalues = np.ones(num_perm, dtype=np.uint64) * np.uint64((1 << 32) - 1)
         self.permutations = self._get_permutation_functions(num_perm, seed)
         
